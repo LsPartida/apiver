7ff259e967b6af0be60cb8ea3d0ceccab7a995f5
Luis Partida <beluis1993@gmail.com>
2020-01-28 09:19:13 -0600

    Version1.0
    -- Backend
    * Funciona
    -- Frontend
    * Funciona

--- Backend/server.js
+++ Backend/server.js
@@ -9,6 +9,8 @@ let precio;
 let sucursal;
 let varprecio;
 let vardescuento;
+let descripcion;
+let preciooferta;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -19,8 +21,7 @@ const pool = mysql.createPool({
   password: process.env.PASSWORD,
   database: process.env.DATABASE
 });
-
-app.get("/", (req, res) => {
+app.get("/cons", (req, res) => {
   let sSql;
   let puntoventa;
   sSql = `SELECT IDPUNTOVENTA,IDSUCURSAL from gequipopc e WHERE EQUIPOPC='${computerName()}'`;
@@ -29,7 +30,7 @@ app.get("/", (req, res) => {
     //Despues de la consulta
     sucursal = rows[0].IDSUCURSAL;
     sSql = `SELECT PRECIOPRED FROM gpuntoventa WHERE IDPUNTOVENTA=${rows[0].IDPUNTOVENTA} LIMIT 1`;
-    console.log({ sucursal, sSql });
+    // console.log({ sucursal, sSql });
     pool.query(sSql, (err, rows) => {
       //Despues de la consulta
       precio = rows[0].PRECIOPRED;
@@ -37,29 +38,62 @@ app.get("/", (req, res) => {
     //Antes de la consulta
   });
   // Antes de la consulta
-  res.send("OK");
+  res.status(200).send("OK");
 });
-
-app.post("/", (req, res) => {
+app.post("/cons", (req, res) => {
   let resul = "";
-  varprecio = ` pespecial(au.PRECIO1,0,au.IDUNIDAD,a.LINEA01,a.IDARTICULO,auc.CODIGOBARRAS,${sucursal}) PRECIO1 `;
-  vardescuento = ` despecial( au.PRECIO1,0,au.IDUNIDAD,a.LINEA01,a.IDARTICULO,auc.CODIGOBARRAS,${sucursal}) DESCUENTO `;
-  sSql = `SELECT IDARTICULO,${varprecio},DESCRIPCION,${vardescuento} FROM garticulos a INNER JOIN garticulosunidades au USING(IDARTICULO) INNER JOIN garticulosunidadescodigos auc USING(IDARTICULO,IDUNIDAD) WHERE auc.CODIGOBARRAS='${req.body.CODIGOBARRAS}'`;
-  // connection.connect();
-  pool.query(sSql, (err, rows) => {
-    if (err) throw err;
-    // Despues del Query
-    // Calcular para mostrar resultados
-    resul = {
-      precio: rows[0].PRECIO1,
-      descripcion: rows[0].DESCRIPCION,
-      preciooferta: rows[0].PRECIO1 * ((100 - rows[0].DESCUENTO) / 100)
-    };
-    console.log({ rows, resul });
-  });
-  // Antes del Query
-  //  console.log({varprecio,vardescuento,sSql})
-  res.send("OK");
+  let idarticulo;
+  varprecio = ` pespecial(au.PRECIO${precio},0,au.IDUNIDAD,a.LINEA01,a.IDARTICULO,auc.CODIGOBARRAS,${sucursal}) PRECIO1 `;
+  vardescuento = ` despecial(au.PRECIO${precio},0,au.IDUNIDAD,a.LINEA01,a.IDARTICULO,auc.CODIGOBARRAS,${sucursal}) DESCUENTO `;
+  sSql = `SELECT IDARTICULO,${varprecio},DESCRIPCION,${vardescuento} FROM garticulos a INNER JOIN garticulosunidades au USING(IDARTICULO) INNER JOIN garticulosunidadescodigos auc USING(IDARTICULO,IDUNIDAD) WHERE auc.ELIMINADO=FALSE and au.ELIMINADO=FALSE and a.ACTIVO=TRUE and au.VENTA=TRUE AND auc.CODIGOBARRAS='${req.body.CODIGOBARRAS}'`;
+  console.log(sSql);
+  if (req.body !== undefined) {
+    pool.query(sSql, (err, rows) => {
+      if (err) {
+        console.log(err);
+        throw err;
+      }
+      if (rows.length !== 0) {
+        descripcion = rows[0].DESCRIPCION;
+        descuento = rows[0].DESCUENTO.toFixed(2);
+        resul = {
+          precio: rows[0].PRECIO1.toFixed(2),
+          descripcion: descripcion
+        };
+        idarticulo = rows[0].IDARTICULO;
+        sSql = `SELECT imp.TIPOFACTOR,imp.TASAOCUOTA
+        FROM garticulosimpuestos art INNER JOIN glasientoimpuestotipo33 imp ON art.IDIMPUESTO=imp.IDASIENTOIMPUESTOTIPO
+        WHERE art.ELIMINADO=FALSE AND art.INCLALVENDER=FALSE AND imp.ACTIVO=TRUE AND imp.C_IMPUESTO IN(002,003) AND imp.TRASLADO=TRUE AND imp.RETENCION=FALSE AND NOT imp.TIPOFACTOR = 'EXENTO' AND IDARTICULO=${idarticulo}`;
+        pool.query(sSql, (err, rows) => {
+          if (rows.length !== 0) {
+            rows.map(imp => {
+              switch (imp.TIPOFACTOR) {
+                case "TASA":
+                  // console.log(imp.TASAOCUOTA)
+                  resul.precio *= imp.TASAOCUOTA + 1;
+                  break;
+                case "CUOTA":
+                  resul.precio += imp.TASAOCUOTA + 1;
+              }
+              resul.precio = resul.precio.toFixed(2);
+            });
+            resul = {
+              ...resul,
+              descuento: descuento,
+              preciooferta: (resul.precio * ((100 - descuento) / 100)).toFixed(
+                2
+              )
+            };
+          } else {
+          }
+          console.log(resul);
+          res.status(200).send(resul);
+        });
+      } else res.status(201).send({ descripcion: "Artículo no encontrado" });
+    });
+    // Antes del Query
+    //  console.log({varprecio,vardescuento,sSql})
+  }
 });
 app.listen(8080, () => {
   console.log(`Server corriendo ${process.env.DBUSERNAME}`);
