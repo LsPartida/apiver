dd4f73e09c9517199478cdc0edfd0d2afba71872
Luis Partida <beluis1993@gmail.com>
2020-01-28 09:19:13 -0600

    Version1.1
    * Se corrigió un problema de rendimiento en el cual, hacía la solicitud sin tener el código de barras

--- frontend/src/Components/Landing.jsx
+++ frontend/src/Components/Landing.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "../../node_modules/bootstrap/dist/css/bootstrap.css";
 import "./Styles/Landing.css";
+const axios = require("axios").default;
 class Landing extends Component {
   state = {
     CODIGOBARRAS: "",
@@ -11,60 +12,72 @@ class Landing extends Component {
   };
 
   handleSubmit = e => {
-    let conf = {
-      method: "POST",
-      body: JSON.stringify(this.state),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    };
     e.preventDefault();
-
-    fetch("localhost", conf).then(response => {
-      response.json();
-      console.log(response);
-    });
+    axios
+      .post(`/cons`, {
+        CODIGOBARRAS: this.state.CODIGOBARRAS
+      })
+      .then(response => {
+        console.log({ status: response.status, datos: response.data });
+        if (response.status === 201)
+          this.setState({
+            CODIGOBARRAS: "",
+            OFERTA: "",
+            DESCUENTO: "",
+            PRECIO: "",
+            DESCRIPCION: response.data.descripcion
+          });
+        else {
+          if (response.data.descuento !== "0.00" && response.data.descuento !== undefined) {
+            this.setState({
+              CODIGOBARRAS: "",
+              DESCUENTO: `${response.data.descuento}% de descuento`,
+              DESCRIPCION: response.data.descripcion,
+              PRECIO: `Precio: $${response.data.precio}`,
+              OFERTA: `Usted paga solamente: $${response.data.preciooferta}`
+            });
+          } else
+            this.setState({
+              CODIGOBARRAS: "",
+              DESCUENTO: ``,
+              DESCRIPCION: response.data.descripcion,
+              PRECIO: `Precio: $${response.data.precio}`,
+              OFERTA: ``
+            });
+        }
+      });
   };
-  handleLoad = e => {
-    // let cabeceras = new Headers();
-    // let conf = {
-    //   method: "GET",
-    //   headers: {'Access-Control-Allow-Origin':'*'},
-    //   mode: "cors",
-    //   cache: "default"
-    // };
-    // fetch("http://localhost:8080",conf).then(response => {
-    //   response.json();
-    //   console.log("GET");
-    // });
-  };
-
   handleChange = e => {
-    e.preventDefault();
     this.setState({
       CODIGOBARRAS: e.target.value
     });
+    // if (e.target.value.length >= 13) this.handleSubmit(e);
   };
+  componentDidMount() {
+    fetch("/cons");
+  }
   render() {
     return (
       <div className="container">
         <div id="uno" className="row">
-          <div id="dos" className="col-lg-2 col-md-2 col-sm-2 col-xs-2"></div>
-          <div id="tres" className="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-            <h1 id="elH1">Ingrese código de barras</h1>
+          <div id="dos" className="col-2"></div>
+          <div id="tres" className="col-6">
+            <h1 className="elH1">Ingrese código de barras</h1>
             <form onSubmit={this.handleSubmit}>
               <input
                 type="text"
-                id="Barcode"
+                placeholder="Ingrese aquí el código"
+                className="Barcode mb-5 mt-5"
                 autoFocus
                 onChange={this.handleChange}
+                value={this.state.CODIGOBARRAS}
               />
             </form>
-            <h1 id="descripcion"></h1>
-            <h1 id="oferta"></h1>
-            <h1 id="descuento"></h1>
-            <h1 id="precio"></h1>
-            <p id="Resultados"></p>
+            <h1 className="descripcion mt-5">{this.state.DESCRIPCION}</h1>
+            <h2 className="precio">{this.state.PRECIO}</h2>
+            <h2 className="descuento">{this.state.DESCUENTO}</h2>
+            <h2 className="oferta">{this.state.OFERTA}</h2>
+            <p className="resultados">Powered By erpDOZ v3.0.2001.5</p>
           </div>
         </div>
       </div>
